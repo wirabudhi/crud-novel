bc089f91e710a3e19139feff375d794f0bdfe893
wirabudhi <bpantaiselatan@gmail.com>
2023-03-26 09:45:30 +0800

    new file:   PROJECT CRUD NOVEL MENGGUNAKAN FRAMEWORK LARAVEL.docx 	modified:   app/Http/Controllers/NovelController.php 	new file:   postcss.config.js 	new file:   public/favicon.ico 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   tailwind.config.js 	new file:   vite.config.js

--- app/Http/Controllers/NovelController.php
+++ app/Http/Controllers/NovelController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Novel;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Novel\StoreRequest;
